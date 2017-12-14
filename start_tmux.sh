80819a18928ebcb1830c6e14acbc2e2b30ca79ae
Gordon Guthrie <gordon@hypernumbers.com>
2017-12-14 09:56:53 +0000

    fix up start file

--- start_tmux.sh
+++ start_tmux.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-tmux new-session -s development
-tmux split-window -v -t development
-tmux split-window -h -t development
+tmux new-session -s dev -n editor -d
+tmux split-window -h -t dev
+tmux split-window -v -t dev
+tmux attach
